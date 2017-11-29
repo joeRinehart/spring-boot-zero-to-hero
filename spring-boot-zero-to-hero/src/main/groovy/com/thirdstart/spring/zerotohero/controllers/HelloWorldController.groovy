c56d9f3f86ecad5cc439b88cbbb214e0d05c5101
Joe Rinehart <joe.rinehart@gmail.com>
2017-11-28 19:14:50 -0500

    Small mapping update

--- spring-boot-zero-to-hero/src/main/groovy/com/thirdstart/spring/zerotohero/controllers/HelloWorldController.groovy
+++ spring-boot-zero-to-hero/src/main/groovy/com/thirdstart/spring/zerotohero/controllers/HelloWorldController.groovy
@@ -6,18 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping
 import java.util.concurrent.atomic.AtomicLong
 
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
-@RequestMapping("/hello-world")
 class HelloWorldController {
 
     static final String template = "Hello, %s!"
     final AtomicLong counter = new AtomicLong()
 
-    @GetMapping
+    @GetMapping('/hello-world')
     @ResponseBody Greeting sayHello(
             @RequestParam(value="name", required=false, defaultValue="Stranger") String name
     ) {
